7358b8c0d4f9cd9a9a62595dfeaa8138aa54ff86
HOSEINLOL <184541064+hoseinlolready@users.noreply.github.com>
2025-07-19 22:28:48 +0330

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,4 +2,5 @@ echo installing the fake Uploader
 apt update
 apt install python3 -y
 apt install wget -y
-wget
+wget https://raw.githubusercontent.com/hoseinlolready/Fake-Upload/refs/heads/main/fake_uploader.py
+echo installed successfully 
